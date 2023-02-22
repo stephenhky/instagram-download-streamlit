71e778f50cf2ff0d42d5410067b057109c209c14
Kwan-Yuet Ho <stephenhky@yahoo.com.hk>
2023-02-22 15:04:07 -0500

    improved website

--- main.py
+++ main.py
@@ -23,16 +23,25 @@ submitbutton = st.button('Submit')
 
 if submitbutton:
     # get shortcode
-    matcher = re.match('https://www.instagram.com/p/([A-Za-z0-9\_]+)', posturl)
+    matcher = re.match('https://www.instagram.com/p/([A-Za-z0-9\_\-]+)', posturl)
     if matcher is not None:
         shortcode = matcher.group(1)
         logging.info('shortcode: {}'.format(shortcode))
         post = instaloader.Post.from_shortcode(L.context, shortcode)
+        st.text('Profile: {} | {}'.format(post.profile, post.owner_profile.biography))
+        st.text(post.caption)
+        check_direct_url = True
         for node in post.get_sidecar_nodes():
+            check_direct_url = False
             logging.info(node.display_url)
             img = Image.open(urllib.request.urlopen(node.display_url))
             st.image(img, width=200)
-            st.components.v1.html('<a href="{}"><img src="{}"></a>'.format(node.display_url, node.display_url))
+            st.components.v1.html('<a href="{}" target="_blank" rel="noreferrer noopener">Click to Open</a>'.format(node.display_url, node.display_url))
+        if check_direct_url:
+            logging.info(post.url)
+            img = Image.open(urllib.request.urlopen(post.url))
+            st.image(img, width=200)
+            st.components.v1.html('<a href="{}" target="_blank" rel="noreferrer noopener">Click to Open</a>'.format(post.url, post.url))
     else:
         st.warning('Invalid URL!')
 
